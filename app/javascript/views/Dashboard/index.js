c013e9927a5f4f63ab313760b383424fc6513f1a
Dhanapal <468739@CTS.COM>
2016-05-20 16:34:06 +0530

    Dashboard style updates

--- app/javascript/views/Dashboard/index.js
+++ app/javascript/views/Dashboard/index.js
@@ -1,8 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { Paper } from 'material-ui';
-import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui';
+import {Paper, Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn, Tabs, Tab} from 'material-ui';
+import FontIcon from 'material-ui/FontIcon';
 class Dashboard extends Component {
   static contextTypes = {
     history: PropTypes.object.isRequired
@@ -10,197 +10,150 @@ class Dashboard extends Component {
  
  
 
-  getStyles() {
-    return {
-      paper: {
-        height: 100,
-        width: 200,
-        marginRight: 26,
-        marginBottom:10,
-        color:'#fff',
-        background:'#ff8a65',
-        textAlign: 'center',
-        display: 'inline-block'
-      }
-    };
-  }
-
-  widgetPrepare(){
-    return{
-      spacing:{
-        marginRight: 20
-      }
-    };
-  }
-
-  dashboardCountsStyle() {
-    return {
-      dashboardWidgets: {
-        marginLeft: 20,
-        position: 'absolute'        
-      } 
-    };
-  }
-
- widgetBlock(){
-    return {
-      widgetBlock:{
-        border:'1px solid #ddd',
-        padding:5,
-        float:'left',
-        width:'70%',
-        boxShadow:'0 1px 1px rgba(0,0,0,0.05)'
-      }
-    };
- } 
-
- widgetBlock25(){
-    return {
-        style:{
-          border:'1px solid #ddd',
-          padding:5,
-          width:'20%',
-          float:'left',
-          marginLeft:5,
-          boxShadow:'0 1px 1px rgba(0,0,0,0.05)'
-        }
-    };
- } 
-
- panelTitle(){
-    return {
-      heading:{
-        fontSize:15
-      }
-    };
- }
-
-panelHeading(){
-  return {
-    header:{       
-      borderBottom: '1px solid #ccc'           
-    }
-  };
-}
-
-dasboardStyle(){
-  return {
-    presets:{
-      fontFamily:'Trebuchet MS',
-      fontSize:13
-    }
-  }
-}    
-
+   
   render() {
 
-     
+   const ReactHighcharts = require('react-highcharts');   
    const { history } = this.context;
-   const styles = this.getStyles();
-   const widgetstyle = this.dashboardCountsStyle();
-   const widgetBlock = this.widgetBlock();
-   const panelHeading = this.panelHeading();
-   const panelTitle = this.panelTitle();
-   const dasboardStyle = this.dasboardStyle();
-   const widgetBlock25 = this.widgetBlock25();
+   const attendanceChartConfig = {
+        chart: {
+            type: 'column',
+            height: 300,
+            width: 540
+        },
+        credits:{
+          enabled:false
+        },
+        title: {
+            text: 'Attendance Report'
+        },
+        subtitle: {
+            text: 'Daily Report'
+        },
+        xAxis: {
+            categories: [
+                '17/05',
+                '18/05',
+                '19/05',
+                'Today'
+                
+            ],
+            crosshair: true
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Attendance Count'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} </b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'Teacher',
+            data: [4, 13, 7, 12]
+
+        }, {
+            name: 'Students',
+            data: [25,28,28,32]
+
+        }]
+    }; 
+
+    const feeReportConfig = {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Fee Reports'
+        },
+        xAxis: {
+            categories: ['Batch 1','Batch 2','Batch 3','Batch 4']
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: 'John',
+            data: [500, 300, 400, 700]
+        }, {
+            name: 'Jane',
+            data: [200, -200, -300, 200]
+        }, {
+            name: 'Joe',
+            data: [-300, 400, 400, -200]
+        },{
+            name: 'Paul',
+            data: [500, 300, 400, 700]
+        }, {
+            name: 'Varn',
+            data: [200, -200, -300, 200]
+        }, {
+            name: 'Doe',
+            data: [-300, 400, 400, -200]
+        }]
+    };
     return (
-        <div style={dasboardStyle.presets}>
-          <Paper style={styles.paper} zDepth={1} rounded={false} >
-            <div style={widgetstyle.dashboardWidgets}>
-               
-              <h2>35</h2>Students
+        <div className="presets">
+          <Paper className="paper wdiget1" zDepth={1} rounded={false} >
+            <div className="dashboardWidgets">
+                
+              <div className="widegetData"><h2>35</h2>Students</div>
+              <div className="widgetIcon"><i className="material-icons">local_library</i></div>
             </div>
           </Paper>
-          <Paper style={styles.paper} zDepth={1} rounded={false}> 
-            <div style={widgetstyle.dashboardWidgets}>
+          <Paper className="paper wdiget2" zDepth={1} rounded={false}> 
+            <div className="dashboardWidgets">
                
-              <h2>43</h2>Teachers
+              <div className="widegetData"><h2>43</h2>Teachers</div>
+              <div className="widgetIcon"><i className="material-icons">people</i></div>
             </div>
           </Paper>
-          <Paper style={styles.paper} zDepth={1} rounded={false}>
-          <div style={widgetstyle.dashboardWidgets}>
+          <Paper className="paper wdiget3" zDepth={1} rounded={false}>
+          <div className="dashboardWidgets">
                
-              <h2>12</h2>Batches
+              <div className="widegetData"><h2>12</h2>Batches</div>
+              <div className="widgetIcon"><i className="material-icons">class</i></div>
             </div>
           </Paper>
-          <Paper style={styles.paper} zDepth={1} rounded={false}>
-          <div style={widgetstyle.dashboardWidgets}>
+          <Paper className="paper wdiget4" zDepth={1} rounded={false}>
+          <div className="dashboardWidgets">
                
-              <h2>73</h2>Courses
+              <div className="widegetData"><h2>73</h2>Courses</div>
+              <div className="widgetIcon"><i className="material-icons">assignment</i></div>
             </div>
           </Paper>
-          <div style={widgetBlock.widgetBlock}>
-              <div style={panelHeading.header} >
-                <h2 style={panelTitle.heading}>Task Manager</h2>
-              </div>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHeaderColumn>Description</TableHeaderColumn>
-                    <TableHeaderColumn>Priority</TableHeaderColumn>
-                    <TableHeaderColumn>Date</TableHeaderColumn>
-                    <TableHeaderColumn>Assigned To</TableHeaderColumn>
-                    <TableHeaderColumn>Status</TableHeaderColumn>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  <TableRow>
-                    <TableRowColumn>Teacher Mapping</TableRowColumn>
-                    <TableRowColumn>High</TableRowColumn>
-                    <TableRowColumn>10 May 2016</TableRowColumn>
-                    <TableRowColumn>John</TableRowColumn>
-                    <TableRowColumn>Assigned</TableRowColumn>
-                  </TableRow>
-                  <TableRow>
-                    <TableRowColumn>Teacher Mapping</TableRowColumn>
-                    <TableRowColumn>High</TableRowColumn>
-                    <TableRowColumn>10 May 2016</TableRowColumn>
-                    <TableRowColumn>John</TableRowColumn>
-                    <TableRowColumn>Assigned</TableRowColumn>
-                  </TableRow>
-                  <TableRow>
-                    <TableRowColumn>Teacher Mapping</TableRowColumn>
-                    <TableRowColumn>High</TableRowColumn>
-                    <TableRowColumn>10 May 2016</TableRowColumn>
-                    <TableRowColumn>John</TableRowColumn>
-                    <TableRowColumn>Assigned</TableRowColumn>
-                  </TableRow>
-                   
-                </TableBody>
-              </Table>
-          </div>           
-           <div style={widgetBlock25.style}>
-              <div style={panelHeading.header} >
-                <h2 style={panelTitle.heading}>Todo List</h2>
-              </div>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHeaderColumn>Todo</TableHeaderColumn>
-                     
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  <TableRow>
-                    <TableRowColumn>Teacher Mapping</TableRowColumn>
-                     
-                  </TableRow>
-                  <TableRow>
-                    <TableRowColumn>Teacher Mapping</TableRowColumn>
-                    
-                  </TableRow>
-                  <TableRow>
-                    <TableRowColumn>Teacher Mapping</TableRowColumn>
-                     
-                  </TableRow>
-                   
-                </TableBody>
-              </Table>
-          </div> 
+         
+           
+          <div className="widgetBlock">
+              <Tabs>
+                <Tab label="Activity" value="a" >
+                  <div>
+                     <ReactHighcharts config = {attendanceChartConfig}></ReactHighcharts>
+                  </div>
+                </Tab>
+                <Tab label="Fee Report" value="b">
+                  <div>
+                     <ReactHighcharts config = {feeReportConfig}></ReactHighcharts>
+                  </div>
+                </Tab>
+              </Tabs>
+          </div>
         </div>
     );
   }
 }
 
 export default connect()(Dashboard);
-
+ 
